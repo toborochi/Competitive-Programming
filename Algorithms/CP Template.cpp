6545e65e6cd4b56879907082271c6bbbc7fe9642
Leonardo H. Añez Vladimirovna <nozosimo@gmail.com>
2017-02-18 22:28:21 -0400

    Create CP Template.cpp
    Competitive Programming Template.

---
+++ Algorithms/CP Template.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define IOS                 ios::sync_with_stdio(0);
+#define ll                  long long
+#define pb                  push_back
+#define par(a,b)            make_pair(a,b)
+#define pfor(a,b,name)      for(int i=a;i<=b;++i){cout<<name[i]<<" ";}cout<<'\n';
+#define ifor(a,b,name)      for(int i=a;i<=b;++i){cin>>name[i];}
+#define euclid(x1,y1,x2,y2) sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1))
+#define lcm(a,b)            (a*b)/(__gcd(a,b))
+#define gcd(a,b)            __gcd(a,b)
+#define dbg()               cout<<"umu"<<endl;
+
+using namespace std;
+
+const double pi = 3.14159265;
+const int INF = 1<<30;
+const int maxn = 1e6;
+const int mod = (1e9)+7;
+const int lim = (1e5);
+
+int main()
+{
+    IOS
+    cin.tie(0);
+    ///freopen("in.txt","r",stdin);
+    ///freopen("out.txt","w",stdout);
+}
+/// This code is less virgin than me
