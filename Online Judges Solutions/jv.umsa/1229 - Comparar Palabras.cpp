982f0da93d27ed42b2af296d267b78dea8c5bf7f
Leonardo H. Añez Vladimirovna <nozosimo@gmail.com>
2018-03-04 15:10:32 -0400

    Create 1229 - Comparar Palabras.cpp

---
+++ Online Judges Solutions/jv.umsa/1229 - Comparar Palabras.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define IOS ios::sync_with_stdio();
+using namespace std;
+int main()
+{
+    IOS
+    string a,b;
+    cin>>a>>b;
+    if(a>b){cout<<a<<" > "<<b<<endl;}else
+    if(a<b){cout<<a<<" < "<<b<<endl;}else
+    cout<<a<<" = "<<b<<endl;
+    return 0;
+}
+ 
+/**************************************************************
+    Problem: 1229
+    User: Vlada
+    Language: C++
+    Result: Accepted
+    Time:0 ms
+    Memory:1568 kb
+****************************************************************/
